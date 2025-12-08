48b70b3e080d909ecad1604e091175bc80a94467
tanisha533 <140799888+tanisha533@users.noreply.github.com>
2025-12-08 23:08:01 +0530

    Time: 14 ms (64.72%), Space: 47.6 MB (69.05%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,34 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-           HashMap<Integer, Integer> freqMap = new HashMap<>();
+         HashMap<Integer, Integer> freqMap = new HashMap<>();
         for (int n : nums) {
             freqMap.put(n, freqMap.getOrDefault(n, 0) + 1);
         }
 
-        // Step 2: Create buckets (index = frequency)
-        List<Integer>[] buckets = new List[nums.length + 1];
-        for (int key : freqMap.keySet()) {
-            int freq = freqMap.get(key);
-            if (buckets[freq] == null) {
-                buckets[freq] = new ArrayList<>();
-            }
-            buckets[freq].add(key);
-        }
+        // Max Heap sorting by frequency value descending
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap =
+      new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
 
-        // Step 3: Collect top K elements
-        List<Integer> resultList = new ArrayList<>();
-        for (int i = buckets.length - 1; i >= 0 && resultList.size() < k; i--) {
-            if (buckets[i] != null) {
-                resultList.addAll(buckets[i]);
-            }
-        }
+        maxHeap.addAll(freqMap.entrySet());
 
-        // Step 4: Convert to int[]
         int[] result = new int[k];
         for (int i = 0; i < k; i++) {
-            result[i] = resultList.get(i);
+            result[i] = maxHeap.poll().getKey();
         }
 
         return result;
+        
     }
 }
