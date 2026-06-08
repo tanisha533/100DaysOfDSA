92eea551a1f1b4f83d2aa91d03c606d33d8510df
tanisha533 <140799888+tanisha533@users.noreply.github.com>
2026-06-08 14:04:48 +0530

    Time: 31 ms (82.96%), Space: 59.2 MB (58.75%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int n=nums.length;
+         for (int i = 0; i < n; i++) {
+            //skip the duplicates for the first number
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int j = i + 1;
+            int k = n - 1;
+        while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0)
+                    k--;
+                else if (sum < 0)
+                    j++;
+              else {
+ //adding Arrays.asList to add tripplet to list of list
+     ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                 j++;
+                 k--;
+            //skip duplicates for the second number
+              while (j < k && nums[j] == nums[j - 1]) { 
+                j++;
+                  
+                }
+            }
+        }
+         }
+        
+         return ans;
+    }
+}
