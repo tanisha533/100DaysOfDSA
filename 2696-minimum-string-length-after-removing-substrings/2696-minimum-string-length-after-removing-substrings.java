9d936fd404d49925a1298cc80b58a370c924caf9
tanisha533 <140799888+tanisha533@users.noreply.github.com>
2026-06-30 14:48:30 +0530

    Time: 7 ms (15.15%), Space: 46.6 MB (7.12%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -1,24 +1,19 @@
 class Solution {
     public int minLength(String s) {
-        //String target = "abc";
-// int start = sb.indexOf(target);
-// if (start != -1) {
-//     sb.delete(start, start + target.length());
-// }   
-    StringBuilder sb=new StringBuilder();
-    for(int i=0; i<s.length(); i++){
-        sb.append(s.charAt(i));
-    }
-    while(sb.indexOf("AB") != -1 || sb.indexOf("CD") != -1 ){
-        if(sb.indexOf("AB") != -1){
-        int start = sb.indexOf("AB");
-         sb.delete(start, start+2);
+    StringBuilder stack = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            stack.append(ch);
+
+            int n = stack.length();
+            if (n >= 2) {
+                String lastTwo = stack.substring(n - 2);
+                if (lastTwo.equals("AB") || lastTwo.equals("CD")) {
+                    stack.delete(n - 2, n);
+                }
+            }
         }
-        if(sb.indexOf("CD") != -1){
-            int nstart = sb.indexOf("CD");
-         sb.delete(nstart, nstart+2);
-        }
-    }
-     return sb.length();
+
+        return stack.length();
     }
 }
