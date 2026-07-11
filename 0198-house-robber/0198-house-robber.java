dc3c464a21c43ac30ed9f4c83b64183df7639c12
tanisha533 <140799888+tanisha533@users.noreply.github.com>
2026-07-11 22:48:42 +0530

    Time: 0 ms (100%), Space: 42.9 MB (37.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
-        //tabulation
-       int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return solve(nums, dp);
-    }
-    public int solve(int[] nums, int[] dp) {
+        //Space Optimisation
           int n=nums.length;
           if (n == 0) return 0;
           if (n == 1) return nums[0];
-           dp[0]=nums[0];
+           int prev2=nums[0];
 
-           dp[1] = Math.max(nums[0], nums[1]);
+           int prev = Math.max(nums[0], nums[1]);
         
         for(int i=2; i<nums.length; i++){
-        int rob = nums[i] + dp[i-2] ;
-        int skip = dp[i-1];
+        int rob = nums[i] + prev2 ;
+        int skip = prev;
         
 
-        dp[i] = Math.max(rob, skip); }
-        return dp[n-1];
-    }
+        int curr = Math.max(rob, skip);
+        prev2=prev; prev=curr; 
+        }
+        return prev;
+          }
 }
