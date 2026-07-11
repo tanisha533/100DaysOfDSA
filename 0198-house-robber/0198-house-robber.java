eb2dbebffb53f33b930b176d91adac8a3b719435
tanisha533 <140799888+tanisha533@users.noreply.github.com>
2026-07-11 22:45:34 +0530

    Time: 0 ms (100%), Space: 42.7 MB (74.6%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
-        //Memoization
+        //tabulation
        int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
-        return solve(0, nums, dp);
+        return solve(nums, dp);
     }
-    public int solve(int i, int[] nums, int[] dp) {
-          if (i >= nums.length)
-            return 0;
+    public int solve(int[] nums, int[] dp) {
+          int n=nums.length;
+          if (n == 0) return 0;
+          if (n == 1) return nums[0];
+           dp[0]=nums[0];
 
-        if (dp[i] != -1)
-            return dp[i];
+           dp[1] = Math.max(nums[0], nums[1]);
+        
+        for(int i=2; i<nums.length; i++){
+        int rob = nums[i] + dp[i-2] ;
+        int skip = dp[i-1];
+        
 
-        int rob = nums[i] + solve(i + 2, nums, dp);
-        int skip = solve(i + 1, nums, dp);
-
-        return dp[i] = Math.max(rob, skip);
+        dp[i] = Math.max(rob, skip); }
+        return dp[n-1];
     }
 }
